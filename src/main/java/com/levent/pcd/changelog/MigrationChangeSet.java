82d9323351aec49bd542d379378d44c8d8d80896
QiaoZL <qiaozl0713@gmail.com>
2019-09-07 09:30:25 -0500

    __

--- src/main/java/com/levent/pcd/changelog/MigrationChangeSet.java
+++ src/main/java/com/levent/pcd/changelog/MigrationChangeSet.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,62 +28,70 @@ import com.levent.pcd.repository.UserInfoRepository;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.model.BulkWriteOptions;
 import com.mongodb.client.model.InsertOneModel;
+
 @ChangeLog
 public class MigrationChangeSet {
-	@Autowired ProductRepository productRepository;
-	@Autowired UserInfoRepository userInfoRepository;
-	@Autowired UserAuthRepository userAuthRepository;
-	
+	@Autowired
+	ProductRepository productRepository;
+	@Autowired
+	UserInfoRepository userInfoRepository;
+	@Autowired
+	UserAuthRepository userAuthRepository;
+
 	@Bean
 	@ChangeSet(order = "001", id = "importProductData", author = "G.LI", version = "1")
-	public void change01(MongoTemplate template) throws IOException{
-		
-		String fileName="src/main/resources/data/products.json";
-		File f = new File(fileName);
-		System.out.println(f.exists());
-		
-		System.out.println(template);
-		List<InsertOneModel<Document>> docs = new ArrayList<>();		
-		
-		int count = 0;
-		int batch = 100;
-		MongoCollection<Document> collection = template.getCollection("products");
-		
-		try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
-		      String line;
-		      while ((line = br.readLine()) != null) {
-		         docs.add(new InsertOneModel<>(Document.parse(line)));
-		         count++;
-		         if (count == batch) {
-		           collection.bulkWrite(docs, new BulkWriteOptions().ordered(false));
-		           docs.clear();
-		           count = 0;
-		        }
-		    }
-		}
+	public void change01(MongoTemplate template) {
+		try {
+			String fileName = "data/products.json";
+			File f = new File(getClass().getClassLoader().getResource(fileName).getFile());
+			System.out.println(f.exists());
 
-		if (count > 0) {
-		   collection.bulkWrite(docs, new BulkWriteOptions().ordered(false));
+			System.out.println(template);
+			List<InsertOneModel<Document>> docs = new ArrayList<>();
+
+			int count = 0;
+			int batch = 100;
+			MongoCollection<Document> collection = template.getCollection("products");
+
+			try (BufferedReader br = new BufferedReader(new FileReader(f))) {
+				String line;
+				while ((line = br.readLine()) != null) {
+					docs.add(new InsertOneModel<>(Document.parse(line)));
+					count++;
+					if (count == batch) {
+						collection.bulkWrite(docs, new BulkWriteOptions().ordered(false));
+						docs.clear();
+						count = 0;
+					}
+				}
+			}
+
+			if (count > 0) {
+				collection.bulkWrite(docs, new BulkWriteOptions().ordered(false));
+			}
+			Product p = new Product();
+			p = template.save(p, "products");
+			template.remove(p, "products");
+			template.updateMulti(Query.query(Criteria.where("_id").exists(true)), Update.update("inStore", 1),
+					"products");
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println(e.getMessage());
 		}
-		Product p = new Product();		
-		p = template.save(p,"products");
-		template.remove(p,"products");
-		template.updateMulti(Query.query(Criteria.where("_id").exists(true)),
-				Update.update("inStore", 1),"products");
 	}
+
 	@Bean
 	@ChangeSet(order = "002", id = "addDefaultUser", author = "G.LI", version = "1")
-	public void change02(MongoTemplate template) throws IOException{
+	public void change02(MongoTemplate template) throws IOException {
 		UserInfo adminInfo = UserInfo.builder().nickname("ADMIN").username("admin@admin").build();
 		UserAuth adminAuth = UserAuth.builder().username("admin@admin").password("admin").build();
 		List<UserRole> roles = new ArrayList<UserRole>();
 		roles.add(UserRole.ROLE_USER);
 		roles.add(UserRole.ROLE_ADMIN);
 		adminAuth.setUserRoles(roles);
-		
-		template.insert(adminAuth,"userAuths");
-		template.insert(adminInfo,"userInfos");
-		
-		
+
+		template.insert(adminAuth, "userAuths");
+		template.insert(adminInfo, "userInfos");
+
 	}
 }
